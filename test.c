d15e3da56149d3a2ef96a781615489dd2aaae7a9
CHFahy <ciaran@ciaranfahy.com>
2021-06-08 19:35:07 +0100

    init

---
+++ test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+char * append(char *a, char *b, int asize, int bsize, char *c){
+    asize = asize-1;
+    bsize = bsize-1;
+    for(int i = 0; i <= asize; i++){
+        c[i] = a[i];
+    }
+    for(int i = 0; i <= bsize; i++){
+        c[i + asize] = b[i];
+
+    }
+    return(c);
+    
+}
+int main(void){
+    char a[] = "foo";
+    char b[] = "bar";
+    char c[sizeof(a) + sizeof(b)];
+    append(a, b, sizeof(a), sizeof(b), c);
+    printf("%s\n", c);
+    return(0);
+}
+
